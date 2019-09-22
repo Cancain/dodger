14f56cdd76d7341a0089366b1be3233937f81214
Cancain <eriksson.tomas@gmail.com>
2019-09-22 16:56:53 +0200

    remade structure of the code for player and player movement

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import * as Phaser from "phaser";
-
-interface OOB {
-  up: boolean;
-  down: boolean;
-  right: boolean;
-  left: boolean;
-}
+import Player from "./player";
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
@@ -14,77 +8,25 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 };
 
 export class GameScene extends Phaser.Scene {
-  private square: Phaser.GameObjects.Rectangle & {
-    body: Phaser.Physics.Arcade.Body;
-  };
-
-  private playerSize = 10;
-  private playerMovementSpeed = 800;
-  private playerRadius = this.playerSize * 2;
-  private oob: OOB = {
-    up: false,
-    down: false,
-    right: false,
-    left: false
-  };
   constructor() {
     super(sceneConfig);
   }
 
+  playerCharacter = new Player();
+
   public create() {
-    this.square = this.add.rectangle(
+    this.playerCharacter.model = this.add.rectangle(
       100,
       100,
-      this.playerSize,
-      this.playerSize,
+      this.playerCharacter.getSize(),
+      this.playerCharacter.getSize(),
       0xffffff
     ) as any;
-    this.physics.add.existing(this.square);
+    this.physics.add.existing(this.playerCharacter.model);
   }
 
   public update() {
-    const player = this.square;
-    const cursorKeys = this.input.keyboard.createCursorKeys();
-
-    if (cursorKeys.up.isDown && !this.oob.up) {
-      player.body.setVelocityY(-this.playerMovementSpeed);
-    } else if (cursorKeys.down.isDown && !this.oob.down) {
-      player.body.setVelocityY(this.playerMovementSpeed);
-    } else {
-      player.body.setVelocityY(0);
-    }
-
-    if (cursorKeys.left.isDown && !this.oob.left) {
-      player.body.setVelocityX(-this.playerMovementSpeed);
-    } else if (cursorKeys.right.isDown && !this.oob.right) {
-      player.body.setVelocityX(this.playerMovementSpeed);
-    } else {
-      player.body.setVelocityX(0);
-    }
-
-    if (player.body.y > window.innerHeight - this.playerRadius) {
-      this.oob.down = true;
-    } else {
-      this.oob.down = false;
-    }
-
-    if (player.body.y < 0 + this.playerRadius) {
-      this.oob.up = true;
-    } else {
-      this.oob.up = false;
-    }
-
-    if (player.body.x > window.innerWidth - this.playerRadius) {
-      this.oob.right = true;
-    } else {
-      this.oob.right = false;
-    }
-
-    if (player.body.x < 0 + this.playerRadius) {
-      this.oob.left = true;
-    } else {
-      this.oob.left = false;
-    }
+    this.playerCharacter.movement(this);
   }
 }
 
